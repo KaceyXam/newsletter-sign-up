40d6080fa762c275326db1de3a6c67f833f063ad
kaceykniffen <kaceykniffen@gmail.com>
2023-07-18 21:51:50 -0500

    Begun styling Success page

---
+++ project/src/routes/Success.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+	<main class="success-box">
+		<img src="/images/icon-success.svg" alt="Success Checkmark" />
+		<h1>Thanks for Subscribing!</h1>
+		<p>
+			A confirmation email has been sent to
+			<strong>ash@loremcompany.com</strong>. Please open it and click the button
+			inside to confirm your subscription.
+		</p>
+		<a href="/" class="btn-secondary">Dismiss message</a>
+	</main>
+</template>
+
+<style scoped>
+.success-box {
+	display: flex;
+	gap: 2em;
+	flex-direction: column;
+	max-width: 550px;
+	background: var(--white);
+	padding: 4em;
+	border-radius: 2.5em;
+}
+
+.success-box img {
+	width: 4em;
+}
+
+.success-box h1 {
+	font-size: 4rem;
+}
+</style>
